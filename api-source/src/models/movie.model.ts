1e68e8a2a3bd1744cdac253caf0c787db80c1ec8
Dylan Gimenez <lu9fdn@gmail.com>
2021-08-30 17:26:08 -0300

    Added Movie model, repository and controller.

---
+++ api-source/src/models/movie.model.ts
@@ -0,0 +1,28 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Movie extends Entity {
+  @property({
+    type: 'string',
+    required: true,
+  })
+  name: string;
+
+  @property({
+    type: 'number',
+    id: true,
+    generated: true,
+  })
+  id?: number;
+
+
+  constructor(data?: Partial<Movie>) {
+    super(data);
+  }
+}
+
+export interface MovieRelations {
+  // describe navigational properties here
+}
+
+export type MovieWithRelations = Movie & MovieRelations;
